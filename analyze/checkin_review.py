e9acaba2a406a3b8dd89bddb2c4309dfda6d0a0a
LimYongJoo <whyzoo@sentience.rocks>
2020-04-08 17:11:00 +0900

    real last commit

--- analyze/checkin_review.py
+++ analyze/checkin_review.py
@@ -3,6 +3,11 @@ import pandas as pd
 from config import FILE_DIR
 from utils.timer import Timer
 
+desired_width = 320
+display_max_columns = 20
+pd.set_option('display.width', desired_width)
+pd.set_option('display.max_columns', display_max_columns)
+
 
 def check_review():
     pass
@@ -10,6 +15,14 @@ def check_review():
 
 if __name__ == "__main__":
     time = Timer()
-    time.start('read csv')
-    review_origin = pd.read_csv(FILE_DIR['review']['csv_file_dir'], usecols=['business_id', 'stars', 'date']).dropna()
+    time.start('read review csv')
+    review = pd.read_csv(FILE_DIR['review']['csv_file_dir'], usecols=['business_id', 'stars', 'date']).dropna()
     time.stop()
+
+    time.start('read checkin csv')
+    checkin = pd.read_csv(FILE_DIR['checkin']['csv_file_dir']).dropna()
+    time.stop()
+
+    df = pd.merge(review, checkin, key='business_id', how='left')
+
+    print("df : ", df)
